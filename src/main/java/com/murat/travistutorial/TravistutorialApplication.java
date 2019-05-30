5bcbea679b34c3108b366b069f7e10ace7efd849
M.Murat <mmuratakbiyik@gmail.com>
2019-05-30 20:41:53 +0300

    initial commit

---
+++ src/main/java/com/murat/travistutorial/TravistutorialApplication.java
@@ -0,0 +1,13 @@
+package com.murat.travistutorial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TravistutorialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TravistutorialApplication.class, args);
+	}
+
+}
